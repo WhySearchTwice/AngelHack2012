dbbf37b56b066ea37d51eda111279a450872bb6f
Ansel Santosa <ansel@anselsantosa.com>
2012-12-01 15:06:35 -0800

    Wrote some js

--- static/javascript/history.js
+++ static/javascript/history.js
@@ -1,38 +1,40 @@
+var tree = {
+    windows: {},
+    contains: treeContains,
+    getWindow: treeGetWindow,
+    getTab: treeGetTab
+};
+var pages = {};
+
 /* Initialize */
 (function() {
-    console.log('test');
-})
+    fetchData();
+})();
 
-/**
- * GET data from the server. Wrapper for AJAX
- *
- * @param String url remote target of POST
- * @param Function callback a function to be called on success. Will be passed the request object
- */
-function get(url, callback) {
-    ajax('GET', url, null, callback);
+$.fn.createChild = function(data) {
+    $(this).appendChild('<div class="site"></div>');
+};
+
+function fetchData() {
+    // get data
+    for (var i = 0, l = pages.length; i < l; i++) {
+
+    }
 }
 
-/**
- * AJAX helper function. Do not use this function directly
- *
- * @param String url remote target of POST
- * @param Mixed data String or Object to be POSTed
- * @param Function callback a function to be called on success. Will be passed the request object
- */
-function ajax(method, url, data, callback) {
-    console.log('Sending: ');
-    console.log(data);
-    var request = new XMLHttpRequest();
-        request.open(method, url, true);
-        request.setRequestHeader('Content-Type', 'text/plain');
-        request.onreadystatechange = function () {
-            if (request.readyState == 4 && request.status == 200) {
-                console.log('Response received:');
-                console.log(request);
-                endLogEvent();
-                if (typeof callback === 'function') { callback(request); }
-            }
-        };
-        request.send(JSON.stringify(data));
+function treeContains(key) {
+    for (var i = 0, l = this.windows.length; i < l; i++) {
+        if (typeof this.windows[key] !== 'undefined') {
+            return true;
+        }
+    }
+    return false;
+}
+
+function treeGetWindow(id) {
+    return this.window[id] || false;
+}
+
+function treeGetTab(id) {
+    if (!this.contains(id)) { console.error('Window ID does not exist'); }
 }
