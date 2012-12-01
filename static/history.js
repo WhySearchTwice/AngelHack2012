13b762afb82f1e6f03c029c4f544fd22a025b945
Christopher Gilbert <cjg081@uw.edu>
2012-12-01 13:20:40 -0800

    Ajax request with hardcode.

--- static/history.js
+++ static/history.js
@@ -2,4 +2,11 @@
 (function() {
     console.log('test');
 })
-console.log('test');
+
+$.get(
+	"http://ec2-174-129-49-253.compute-1.amazonaws.com",
+    {user : "anstosa_gmail.com"},
+    function(data) {
+        alert("Data loaded: " + data);
+    }
+);
