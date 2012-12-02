ec5d46fbb600bb21864aa12576fb1709e3487e25
Tony Grosinger <tony@grosinger.net>
2012-12-01 16:35:49 -0800

    Added page parsing

--- static/javascript/history.js
+++ static/javascript/history.js
@@ -1,6 +1,6 @@
 var tree = {
-    windows: {},
-    contains: treeContains,
+    devices: {},
+    getDevice: treeGetDevice,
     getWindow: treeGetWindow,
     getTab: treeGetTab
 };
@@ -10,7 +10,7 @@ var pages = {};
 (function() {
     fetchData();
     testGet();
-    console.log(pages);
+    paseeData();
 })();
 
 $.fn.createChild = function(data) {
@@ -24,21 +24,80 @@ function fetchData() {
     }
 }
 
-function treeContains(key) {
-    for (var i = 0, l = this.windows.length; i < l; i++) {
-        if (typeof this.windows[key] !== 'undefined') {
-            return true;
+function parseData() {
+    for(var objId in pages) {
+        var obj = pages[objId];
+
+        // Ensure device exists
+        var device = tree.getDevice(obj.deviceGuid);
+        if(device == null) {
+            tree.devices[obj.deviceGuid] = {windows: {}};
+            device = tree.getDevice(obj.deviceguid);
         }
+
+        // Ensure window exists
+        var window = tree.getWindow(obj.deviceGuid, obj.windowId);
+        if(window == null) {
+            device.windows[obj.windowId] = {tabs:{}};
+            window = tree.getWindow(obj.deviceGuid, obj.windowId);
+        }
+
+        // Ensure tab exists
+        var tab = tree.getTab(obj.deviceGuid, obj.windowId, obj.tabId);
+        if(tab == null) {
+            window.tabs[obj.tabId] = {pages: {}};
+            tab = tree.getTab(obj.deviceGuid, obj.windowId, obj.tabId);
+        }
+
+        // Create the page in the tab
+        // TODO: This might be cleared whenever the pages variable is reset
+        tab.pages[obj.pageOpenTime] = obj;
     }
-    return false;
 }
 
-function treeGetWindow(id) {
-    return this.window[id] || false;
+/**
+ * Retrieve the device layer of organization from the window object
+ * @Param: String deviceGuid The id of the device that is being searched for
+ * @Return: Object device object or null if it does not exist
+ */
+function treeGetDevice(deviceGuid) {
+    return this.tree[deviceGuid] || null;
 }
 
-function treeGetTab(id) {
-    if (!this.contains(id)) { console.error('Window ID does not exist'); }
+/**
+ * Retrieve the window layer of organization from the window object
+ * @Param: String deviceGuid The id of the device that is being searched for
+ * @Param: String windowId The id of the window that is being searched for
+ * @Return: Object window object or null if it does not exist
+ */
+function treeGetWindow(deviceGuid, windowId) {
+    if (this.tree.getDevice(deviceGuid) == null) {
+        console.error('Device GUID does not exist');
+        return null;
+    }
+
+    return this.tree[deviceGuid].windows[windowId] || null;
+}
+
+/**
+ * Retrieve the tab layer of organization from the window object
+ * @Param: String deviceGuid The id of the device that is being searched for
+ * @Param: String windowId The id of the window that is being searched for
+ * @Param: String tabId The id of the tab that is being searched for
+ * @Return: Object tab object or null if it does not exist
+ */
+function treeGetTab(deviceGuid, windowId, tabId) {
+    if (this.tree.getDevice(deviceGuid) == null) {
+        console.error('Device GUID does not exist');
+        return null;
+    }
+
+    if (this.tree.getWindow(windowId) == null) {
+        console.error('Window does not exist');
+        return null;
+    }
+
+    return this.tree[deviceGuid].windows[windowId].tabs[tabId] || null;
 }
 
 /**
