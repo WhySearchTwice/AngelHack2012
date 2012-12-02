e98a5dc756cbb600a8eda6778d991753d9b890b0
Christopher Gilbert <cjg081@uw.edu>
2012-12-02 11:13:16 -0800

    tooltips

--- static/javascript/history.js
+++ static/javascript/history.js
@@ -331,8 +331,8 @@ function createSVGTooltip(obj, x, y) {
     // Create the tooltip square
     var newNodeRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     newNodeRect.setAttribute("height", "50");
-    newNodeRect.setAttribute("width", 200);
-    newNodeRect.setAttribute("fill", "blue");
+    newNodeRect.setAttribute("width", 400);
+    newNodeRect.setAttribute("fill", "grey");
 
     // Create the text for the tooltip
     var textNode = document.createElementNS("http://www.w3.org/2000/svg", "text");
