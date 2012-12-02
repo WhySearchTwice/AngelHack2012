74c414bcd29823f54c3f822cc8000a0e998888d5
Ansel Santosa <ansel@anselsantosa.com>
2012-12-01 20:33:38 -0800

    Test programatic site addition

--- static/javascript/history.js
+++ static/javascript/history.js
@@ -40,13 +40,13 @@ var ids = {};
 
 function addChild(parent, data, type) {
     $site = $('\
-        <div class="' + type + '" id="' + ids[data.deviceGuid + data.windowId + data.tabId + data.pageOpenTime] + '">\
+        <div class="' + type + '" id="page_' + ids[data.deviceGuid + data.windowId + data.tabId + data.pageOpenTime] + '">\
             <div class="site">' + data.pageUrl + '</div>\
         </div>\
     ');
     if (data.hasOwnProperty('attrs')) {
         for (var i = 0, l = data.attrs.length; i < l; i++) {
-            $site.attr(data.attrs[i], data[data.attrs[i]]);
+            $site.attr(data.attrs[i], 'num_' + data[data.attrs[i]]);
         }
     }
     $(parent).append($site);
@@ -93,15 +93,16 @@ function parseData() {
         var pageId = ids[obj.deviceGuid + obj.windowId + obj.tabId + obj.pageOpenTime] = view.idCounter;
         view.idCounter++;
 
-        if ($('[windowid="' + obj.windowId + '"]').length == 0) {
+        if ($('[windowid="num_' + obj.windowId + '"]').length == 0) {
             obj.attrs = ['windowid'];
             $('#timeline').addBranch(obj);
-        } else if ($('[tabid="' + obj.tabId + '"]') == 0) {
+        } else if ($('[tabid="num_' + obj.tabId + '"]') == 0) {
             obj.attrs = ['tabid'];
-            $('[windowid ="' + obj.windowId + '"]').addBranch(obj);
+            $('[windowid="num_' + obj.windowId + '"]').addBranch(obj);
         } else {
-            $('[tabid="' + obj.tabId + '"]').addStem(obj);
+            $('[tabid="num_' + obj.tabId + '"]').addStem(obj);
         }
+        $('[windowid="num_' + obj.windowId + '"] [tabid="num_' + obj.tabId + '"] .branch').moveInto($('#page_' + pageId));
 
     }
 
